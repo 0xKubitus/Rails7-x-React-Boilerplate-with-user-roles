5ed6fba326bda1e86c148bd94983f993c0a52a92
0xKubitus <n.cubilier@gmail.com>
2022-09-16 03:25:44 +0200

    tadaa

---
+++ my-Authentication-Boilerplate_Rails-API-React-App-main/2.React-frontend_MARKETPLACE/src/App.js
@@ -0,0 +1,13 @@
+import SignUp from './SignUp';
+import Login from './Login';
+
+function App() {
+    return (
+        <main className="App">
+            <SignUp />
+            <Login />
+        </main>
+    );
+}
+
+export default App;
